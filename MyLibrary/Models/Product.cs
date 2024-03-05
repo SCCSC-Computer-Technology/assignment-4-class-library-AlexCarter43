41c7f85d8eb9ed7d095509f74a57ef2c4df097f1
AlexCarter43 <156260672+AlexCarter43@users.noreply.github.com>
2024-03-05 02:11:51 -0500

    Add files via upload

---
+++ MyLibrary/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Models
+{
+    class Product
+    {
+        public string Product_Number { get; set; }
+        public decimal Price { get; set; }
+        public int Units_On_Hand { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MyLibrary/Utilities/PriceFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Utilities
+{
+    class PriceFormatter
+    {
+        public static string FormatPrice(decimal price)
+        {
+            return price.ToString("$0.00");
+        }
+    }
+}
